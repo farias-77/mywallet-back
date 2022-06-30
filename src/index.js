b104dde45b03207b8968d5f7aede536fcee21da1
farias_77 <gabrielfalves2003@gmail.com>
2022-06-30 10:14:32 -0300

    registra entradas e saídas

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { signUpUser, signInUser } from './controllers/userControllers.js';
+import { registerMovement } from './controllers/moneyControllers.js';
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
@@ -9,8 +10,9 @@ server.use(cors());
 server.use(express.json());
 
 
-
 server.post('/sign-up', signUpUser);
 server.post('/sign-in', signInUser);
+
+server.post('/moneyMovement', registerMovement)
 
 server.listen(process.env.PORT);
