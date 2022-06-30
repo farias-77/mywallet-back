8be3f901e533e179d6e731231156b5e317597342
farias_77 <gabrielfalves2003@gmail.com>
2022-06-30 09:36:15 -0300

    request login

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,4 +1,5 @@
 import { db } from '../mongoConfig.js';
+import { v4 as uuid} from 'uuid';
 import bcrypt from 'bcrypt';
 import joi from 'joi';
 
@@ -20,6 +21,32 @@ export async function signUpUser(req, res){
     }
 
     const encryptedPassword = bcrypt.hashSync(password, 10);
-    await db.collection('users').insertOne({ name, email, encryptedPassword });
+    await db.collection('users').insertOne({ name, email, password: encryptedPassword });
     res.status(201).send('Usuário criado');
+}
+
+export async function signInUser(req, res){
+    const { email, password } = req.body;
+
+    const signInSchema = joi.object({
+        email: joi.string().email().required(),
+        password: joi.string().required() 
+    });
+
+    const { error } = signInSchema.validate({ email, password });
+
+    if(joi.isError(error)){
+        res.status(400).send('Login inválido, por favor verifique.');
+    }
+
+    const user = await db.collection('users').findOne({ email });
+
+    if(user && bcrypt.compareSync(password, user.password)){
+        const token = uuid();
+
+        await db.collection('sessions').insertOne({ userId: user._id, token });
+        res.status(201).send(token);
+    }
+    
+    res.status(401).send('Usuário ou senha incorretos');
 }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
-import { signUpUser } from './controllers/userControllers.js';
+import { signUpUser, signInUser } from './controllers/userControllers.js';
 import express from 'express';
-import cors from 'cors';
 import dotenv from 'dotenv';
+import cors from 'cors';
 dotenv.config();
-
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 
 
+
 server.post('/sign-up', signUpUser);
-
-
-
-
-
-
+server.post('/sign-in', signInUser);
 
 server.listen(process.env.PORT);
